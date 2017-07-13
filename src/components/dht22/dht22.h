dddb81c5836123f40144a1a846700f89ec7fac6e
ThangLe <thanglenguyen196@gmail.com>
2017-07-13 23:15:33 +0700

    - Create a sample module

---
+++ src/components/dht22/dht22.h
@@ -0,0 +1,5 @@
+#ifndef _DHT22_H
+#define _DHT22_H
+
+
+#endif // _DHT22_H
---
+++ test/components/dht22/test_dht22.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "dht22.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_dht22_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement dht22");
+}
