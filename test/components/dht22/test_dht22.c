2130f7172e50700703eb51c6191c00dca53c428e
ThangLe <thanglenguyen196@gmail.com>
2017-07-14 00:22:47 +0700

    - Add unit test for return code in case invalid arguments passing to DHT decode function

--- test/components/dht22/test_dht22.c
+++ test/components/dht22/test_dht22.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "dht22.h"
+#include <stdint.h>
 
 void setUp(void)
 {
@@ -9,7 +10,27 @@ void tearDown(void)
 {
 }
 
-void test_dht22_NeedToImplement(void)
+// void test_dht22_NeedToImplement(void)
+// {
+//   TEST_IGNORE_MESSAGE("Need to Implement dht22");
+// }
+
+void test_whenInputWrongValueTo__DHT_BitStream_Decode__thenErrorCodeShouldReturned(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement dht22");
+  uint32_t tmpArray[40];
+  dht_data_t dhtValue;
+
+  // All Arguments are invalid
+  TEST_ASSERT_EQUAL(DHT_BIT_STREAM_ERROR, DHT_BitStream_Decode(0, 0, 0));
+
+  // Argument Combinations are invalid
+  TEST_ASSERT_EQUAL(DHT_BIT_STREAM_ERROR, DHT_BitStream_Decode(0, 0, &dhtValue));
+  TEST_ASSERT_EQUAL(DHT_OTHER_ERROR, DHT_BitStream_Decode(0, 40, 0));
+  TEST_ASSERT_EQUAL(DHT_OTHER_ERROR, DHT_BitStream_Decode(0, 40, &dhtValue));
+  TEST_ASSERT_EQUAL(DHT_BIT_STREAM_ERROR, DHT_BitStream_Decode(tmpArray, 0, 0));
+  TEST_ASSERT_EQUAL(DHT_BIT_STREAM_ERROR, DHT_BitStream_Decode(tmpArray, 0, &dhtValue));
+  TEST_ASSERT_EQUAL(DHT_OTHER_ERROR, DHT_BitStream_Decode(tmpArray, 40, 0));
+
+  // Length other than 40 is also an error
+  TEST_ASSERT_EQUAL(DHT_BIT_STREAM_ERROR, DHT_BitStream_Decode(tmpArray, 20, &dhtValue));
 }
